3ec6fb3e0bb5b96e571c941fa7703745e6fc630a
vusdkvus1 <piona9355@naver.com>
2025-04-23 10:34:54 +0900

    feat: Add and Delete

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
-for i in range(1, 18):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i %3 == 0:
+for i in range(1, 14):
+    if i %3 == 0:
         print('fizz')
+    elif i%5 == 0:
+        print('buzz')
     else:
         print(f'{i}')
